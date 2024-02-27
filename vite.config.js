2a24c20084835c3a4727e6577fa10c0e7740e763
sharath <sharath.dt@gmail.com>
2024-02-27 17:54:27 +0530

    remove a vulnerability

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  server: {
-    fs: {
-      allow: [".."],
-    },
-  },
 };
 
 export default config;
